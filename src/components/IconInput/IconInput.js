141705ce677eba5c450f56ec47092c729b3b2e78
Oisín Quinn <oisin1001@gmail.com>
2021-05-16 22:11:01 +0100

    Added IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    "--font-size": "14px",
+    "--border-size": "1px",
+    "--input-padding": "28px"
+  },
+  large: {
+    "--font-size": "18px",
+    "--border-size": "2px",
+    "--input-padding": "32px"
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,67 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const style = STYLES[size];
+
+  return (
+    <Wrapper style={{ "--width": width + "px", ...style }}>
+      <IconWrapper>
+        <StyledIcon id={icon} strokeWidth={size === "large" ? 2 : 1} size={size === "large" ? 24 : 20} />
+        <VisuallyHidden>{label}</VisuallyHidden>
+      </IconWrapper>
+      <Input placeholder={placeholder} />
+    </Wrapper>);
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border-bottom: var(--border-size) solid black;
+  width: var(--width);
+  line-height: 21px;
+`;
+
+const Input = styled.input`
+  border: none;
+  width: 100%;
+  padding: 8px;
+  padding-left: var(--input-padding);
+  font-size: var(--font-size);
+  font-weight: 600;
+  color: ${COLORS.gray700};
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+    padding-left: 92px;
+    opacity: 1;
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const StyledIcon = styled(Icon)`
+  height: 100%;
+  color: ${COLORS.gray700};
+  padding: 2px;
+
+  ${Wrapper}:hover & {
+    color: black;
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0; left: 0; bottom: 0;
+  width: fit-content;
+  height: fit-content;
+  margin: auto;
+  pointer-events: none;
+`;
 
 export default IconInput;
